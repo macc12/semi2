a57be20cc43153590b019827149fd737862023ca
fabian.giraldo <fabian.giraldo@fgiraldo.usa.loc>
2019-04-02 11:58:29 -0500

    Primer Despliegue

--- src/main/java/MainGlassfish.java
+++ src/main/java/MainGlassfish.java
@@ -1,4 +1,13 @@
-import edu.co.sergio.mundo.dao.CreateDataBase;
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author white
+ */
 import java.io.File;
 import org.glassfish.embeddable.CommandResult;
 import org.glassfish.embeddable.CommandRunner;
